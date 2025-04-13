9cff2f9066e7d6aca07fe388e7643c446e13da9c
WWM_Local <walkerwilly02@gmail.com>
2025-04-13 12:02:02 +0200

    Ajout d'une méthode pour récupérer toutes les commandes et d'une pour récupérer la répartition du status des commandes

--- src/DAO/OrdersDAOImpl.java
+++ src/DAO/OrdersDAOImpl.java
@@ -1,16 +1,22 @@
 package DAO;
 
 // DAO OrderDAO.java
+
 import Model.OrdersModel;
 
 import java.sql.*;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class OrdersDAOImpl {
     private DaoFactory daoFactory;
 
     /**
      * Instancie un daoFactory
+     *
      * @param daoFactory
      */
     public OrdersDAOImpl(DaoFactory daoFactory) {
@@ -19,11 +25,12 @@ public class OrdersDAOImpl {
 
     /**
      * crée un nouvelle commande dans la base de donnée
+     *
      * @param order
      */
     public boolean createOrder(OrdersModel order) {
-        try{
-            Connection connexion= daoFactory.getConnection();
+        try {
+            Connection connexion = daoFactory.getConnection();
 
             LocalDateTime rdvFulltime = order.getRdvFulltime();
             int personCount = order.getPersonCount();
@@ -32,7 +39,7 @@ public class OrdersDAOImpl {
             int attractionId = order.getAttractionId();
             int reservationId = order.getReservationId();
 
-            PreparedStatement preparedStatement= connexion.prepareStatement("INSERT INTO orders(rdv_fulltime, person_count, price, status, attraction_id, reservation_id) VALUES ('" + rdvFulltime +"','" + personCount +"','" + price +"','" + status +"','" + attractionId +"','" + reservationId +"')",Statement.RETURN_GENERATED_KEYS);
+            PreparedStatement preparedStatement = connexion.prepareStatement("INSERT INTO orders(rdv_fulltime, person_count, price, status, attraction_id, reservation_id) VALUES ('" + rdvFulltime + "','" + personCount + "','" + price + "','" + status + "','" + attractionId + "','" + reservationId + "')", Statement.RETURN_GENERATED_KEYS);
             preparedStatement.executeUpdate();
             ResultSet generatedKeys = preparedStatement.getGeneratedKeys();
             if (generatedKeys.next()) {
@@ -41,20 +48,67 @@ public class OrdersDAOImpl {
                 System.out.println("Order créé avec ID : " + generatedId);
                 return true;
             }
-        }catch(SQLException e){
+        } catch (SQLException e) {
             e.printStackTrace();
             System.out.println("Ajout de la commande impossible");
             return false;
         }
         return false;
     }
+
+    /**
+     * Récupère à l'aide d'un call au DAO toutes les commandes
+     * @return list , liste de toutes les commandes
+     */
+    public List<OrdersModel> getAllOrders() {
+        List<OrdersModel> list = new ArrayList<>();
+        try {
+            Connection connexion = daoFactory.getConnection();
+            Statement statement = connexion.createStatement();
+
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM orders");
+            while (resultSet.next()) {
+                OrdersModel order = new OrdersModel(resultSet.getInt("order_id"),
+                        resultSet.getTimestamp("rdv_fulltime").toLocalDateTime(),
+                        resultSet.getInt("person_count"),
+                        resultSet.getFloat("price"),
+                        resultSet.getString("status"),
+                        resultSet.getInt("attraction_id"),
+                        resultSet.getInt("reservation_id"));
+                list.add(order);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Récupération des commandes impossible");
+        }
+        return list;
+    }
+    public Map<String, Integer> getStatusCount() {
+        Map<String, Integer> statusMap = new HashMap<>();
+        String sql = "SELECT status, COUNT(*) as total FROM Orders GROUP BY status";
+
+        try (Connection conn = daoFactory.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql);
+             ResultSet rs = stmt.executeQuery()) {
+
+            while (rs.next()) {
+                String status = rs.getString("status");
+                int count = rs.getInt("total");
+                statusMap.put(status, count);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return statusMap;
+    }
+
     public OrdersModel getOrderById(int prOrderId) {
         OrdersModel resultOrder = null;
-        try{
-            Connection connexion= daoFactory.getConnection();
-            Statement statement= connexion.createStatement();
+        try {
+            Connection connexion = daoFactory.getConnection();
+            Statement statement = connexion.createStatement();
 
-            ResultSet resultSet= statement.executeQuery("SELECT * FROM orders WHERE order_id = '" + prOrderId + "'");
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM orders WHERE order_id = '" + prOrderId + "'");
             while (resultSet.next()) {
                 int orderID = resultSet.getInt("order_id");
                 LocalDateTime rdvFulltime = resultSet.getTimestamp("rdv_fulltime").toLocalDateTime();
@@ -64,15 +118,15 @@ public class OrdersDAOImpl {
                 int attractionId = resultSet.getInt("attraction_id");
                 int reservationId = resultSet.getInt("reservation_id");
 
-                if(prOrderId==orderID){
+                if (prOrderId == orderID) {
                     System.out.println("Commande trouvée dans la base de données");
-                    resultOrder=new OrdersModel(orderID,rdvFulltime,personCount,price,status,attractionId,reservationId);
+                    resultOrder = new OrdersModel(orderID, rdvFulltime, personCount, price, status, attractionId, reservationId);
                     break;
                 }
 
             }
 
-        }catch(SQLException e){
+        } catch (SQLException e) {
             e.printStackTrace();
             System.out.println("Ajout de la commande impossible");
         }
@@ -80,20 +134,20 @@ public class OrdersDAOImpl {
     }
 
     public boolean updateOrderStatus(int orderId, String newStatus) {
-        int rowsAffected=0;
-        try{
-            Connection connexion= daoFactory.getConnection();
-            Statement statement= connexion.createStatement();
+        int rowsAffected = 0;
+        try {
+            Connection connexion = daoFactory.getConnection();
+            Statement statement = connexion.createStatement();
 
-            String sql="UPDATE orders SET status = '" + newStatus + "' WHERE order_id = " + orderId;
-            rowsAffected= statement.executeUpdate(sql);
+            String sql = "UPDATE orders SET status = '" + newStatus + "' WHERE order_id = " + orderId;
+            rowsAffected = statement.executeUpdate(sql);
             // Vérification que la mise à jour a bien eu lieu
 
 
             // Fermeture des ressources
             statement.close();
             connexion.close();
-        }catch(SQLException e){
+        } catch (SQLException e) {
             e.printStackTrace();
             System.out.println("Modification du produit impossible");
         }
