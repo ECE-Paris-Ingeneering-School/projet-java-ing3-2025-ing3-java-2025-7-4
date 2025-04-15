c9df9dae56eb612736677d380d5b90c0c1ca4dcc
Erel Rebot <erel.rebot@gmail.com>
2025-04-15 15:07:25 +0200

    Liaison entre le controller et le model pour y ajouter les infos de la réservation.

--- Controller/ReservationController.java
+++ Controller/ReservationController.java
@@ -1,10 +1,12 @@
 package Controller;
 
+import Model.ReservationModel;
 import View.ReservationView;
 import javax.swing.*;
 
 public class ReservationController {
     private ReservationView view;
+    private ReservationModel model = new ReservationModel(2, 0.8) ;
 
     public ReservationController(ReservationView view) {
         this.view = view;
@@ -17,11 +19,11 @@ public class ReservationController {
             int nbEnfant = (Integer) view.getChildrenBox().getSelectedItem();
             int nbBebe = (Integer) view.getBabiesBox().getSelectedItem();
 
+            model.addNumClient(nbAdulte, nbEnfant, nbBebe);
 
-
-            System.out.println("Adulte(s): " + nbAdulte
-                    + ", Enfant(s): " + nbEnfant
-                    + ", Bébé(s): " + nbBebe);
+            System.out.println("Adulte(s): " + model.getNumAdults()
+                    + ", Enfant(s): " + model.getNumKids()
+                    + ", Bébé(s): " + model.getNumBabys());
         });
     }
 
