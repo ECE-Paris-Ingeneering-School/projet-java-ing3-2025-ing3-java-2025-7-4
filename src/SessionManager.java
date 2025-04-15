63e3d6d303999aa4ca4371f50956473b9ec9a6a3
WWM_Local <walkerwilly02@gmail.com>
2025-04-15 21:27:22 +0200

    Ajout de la classe statique SessionManager permet de gérer la session de l'utilisateur et sa connexion au site entre autre

---
+++ src/SessionManager.java
@@ -0,0 +1,51 @@
+//Import des packages nécessaires
+import Model.ClientModel;
+
+/**
+ * Classe qui va gérer la connection ou non d'un utilisateur
+ */
+public class SessionManager {
+    /**
+     * Objet du modèle client contenant ces informations
+     */
+    private static ClientModel currentUser;
+
+    /**
+     * Constructeur de la classe
+     * @param user , utilisateur qui est connecté à la page
+     */
+    public static void setCurrentUser(ClientModel user) {
+        currentUser = user;
+    }
+
+    /**
+     * Getter de l'attribut privé
+     * @return currentUser, l'objet contenant ces informations
+     */
+    public static ClientModel getCurrentUser() {
+        return currentUser;
+    }
+
+    /**
+     * Méthode pour savoir si on est connecté ou invité ou autre
+     * @return true ou false
+     */
+    public static boolean isLoggedIn() {
+        return currentUser != null && currentUser.getClientID() != 7;
+    }
+
+    /**
+     * Permet de savoir si l'utilisateur actuel est un admin
+     * @return true ou false
+     */
+    public static boolean isAdmin() {
+        return isLoggedIn() && currentUser.getRole() == 2;
+    }
+
+    /**
+     * Permet de se déconnecter
+     */
+    public static void logout() {
+        currentUser = null;
+    }
+}
