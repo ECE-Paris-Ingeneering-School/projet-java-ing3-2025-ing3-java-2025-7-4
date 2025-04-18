d44ba0b46c0b0b1ae1540e329335a420197ee12c
juliette <juliettes.ellito@edu.ece.fr>
2025-04-18 16:19:14 +0200

    commentaires finis

--- src/Controller/Attraction/AttractionController.java
+++ src/Controller/Attraction/AttractionController.java
@@ -15,9 +15,19 @@ public class AttractionController {
 
     private final AttractionDAO dao;
 
+    /**
+     * Crée une instance de AttractionDAO (l’objet qui interagit avec la base de données pour les attractions). Et la stocke dans un attribut dao pour qu’on puisse l’utiliser plus tard dans d’autres méthodes du contrôleur (getAllAttractions, addAttraction, etc.).
+     * @param daoFactory est un objet qui permet de créer et configurer les accès à la base de données (DAO)
+     */
     public AttractionController(DaoFactory daoFactory) {
         this.dao = new AttractionDAO(daoFactory);
     }
+
+    /**
+     *
+     * @param map est une structure clé-valeur contenant les données d'une attraction
+     * @return un objet AttractionModel construit à partir des valeurs contenues dans cette map.
+     */
 
     private AttractionModel buildAttractionFromMap(Map<String, Object> map) {
         return new AttractionModel(
@@ -30,17 +40,36 @@ public class AttractionController {
         );
     }
 
-    public List<AttractionModel> getAllAttractions() {
-        return dao.getAllAttractions();
-    }
+    /**
+     *
+     * @return retourne une liste d'attractions.
+     */
 
+        public List<AttractionModel> getAllAttractions() {
+            return dao.getAllAttractions();
+        }
+
+    /**
+     *
+     * @param prAttraction représente un ensemble de données sous forme de paires clé-valeur
+     */
     public void createAttra(Map<String, Object> prAttraction) {
         dao.insertAttraction(buildAttractionFromMap(prAttraction));
     }
 
+    /**
+     *
+     * @param prAttraction ensemble de données sous forme de paires clé-valeur représentant une attraction à mettre à jour.
+     */
+
     public void updateAttra(Map<String, Object> prAttraction) {
         dao.updateAttraction(buildAttractionFromMap(prAttraction));
     }
+
+    /**
+     *
+     * @param prID L'identifiant de l'attraction à supprimer.
+     */
 
     public void deleteAttra(int prID) {
         dao.deleteAttraction(prID);
